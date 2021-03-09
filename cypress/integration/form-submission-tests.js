e5334fcbed45055757d2c1b7d839c33ea58f3f2b
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-03-09 11:34:48 -0700

    Add POST intercept

---
+++ cypress/integration/form-submission-tests.js
@@ -0,0 +1,37 @@
+describe('Form submission', () => {
+  it('Should be able to visit the page using a stubbed GET', () => {
+    cy.fixture('../fixtures/urls.json')
+      .then((response) => {
+        cy.intercept("GET", "http://localhost:3001/api/v1/urls", {
+          statusCode: 200,
+          body: response
+        })
+    })
+
+    cy.intercept("POST", "http://localhost:3001/api/v1/urls", {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        long_url: "http://cypresstesturl/longurl/requesttoshorten",
+        title: "Cypress Test Title"
+      })
+    })
+
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Should allow users to post a new url in the form', () => {
+    cy.get('input[name=title]')
+      .type('Cypress Test Title')
+      .get('input[name=url]')
+      .type('http://cypresstesturl/longurl/requesttoshorten')
+      .get('form button').click()
+  })
+
+  it.skip('Should clear the inputs after form submission', () => {
+
+  })
+
+})
