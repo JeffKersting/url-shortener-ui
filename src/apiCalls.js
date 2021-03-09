dc2ac324269d227c9744c3801ea39cf31d06e308
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-03-09 10:01:24 -0700

    Add post to apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,6 +3,21 @@ const fetchRequests = {
   getUrls: () => {
     return fetch('http://localhost:3001/api/v1/urls')
     .then(response => response.json())
+  },
+
+  postUrl: (url, title) => {
+    const postObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        long_url: url,
+        title: title
+      })
+    }
+    return fetch('http://localhost:3001/api/v1/urls', postObj)
+      .then(response => console.log(response))
   }
 
 }
